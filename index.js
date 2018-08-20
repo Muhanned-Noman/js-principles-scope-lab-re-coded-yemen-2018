30db65cca3b467b891430cf0b02c4b52b1e79ad7
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-08-20 16:32:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName;
+function upperCaseCustomerName(customerName){
+  return customerName.toUpperCase();
+}
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+  bestCustomer = 'Muhanned';
+}
+const leastFavoriteCustomer = 'Ahmed';
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'Ali';
+}
